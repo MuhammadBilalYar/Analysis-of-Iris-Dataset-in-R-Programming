8e818d87fe9cd0132979a0aae0f3a80f13d6cf15
Muhammad Bilal <Muhammad.Bilal@bentley.com>
2018-12-26 20:38:46 +0500

    Added Program.R file

---
+++ Program.R
@@ -0,0 +1,25 @@
+#load 'multigraph' library
+library(multigraph)
+
+#load Iris dataset as matrix in matrix variable
+matrix <- data.matrix(iris)
+
+#matrix transpose beacuse 'cor' method calcualte column wise correlation 
+trn<-t(matrix)
+
+#calcaulate pairwise correlation (150*150) in pwcor variable
+pwcor<-cor(trn)
+
+#save calculated (150*150) correlation in csv file
+write.csv(pwcor, file = "correlation.csv")
+
+# define scope of node / edge / graph characteristics as list object
+scp3 <- list(cex = 1, fsize = 3, pch = c(19, 15), lwd = 1.5, vcol = 2:3)
+
+#Map pairwise correlation (150*150) to 'circ2' layout base graph
+bmgraph(pwcor, layout = "circ2", scope = scp3, main = "Iris correlation using bmgraph")
+
+#calculate mean, median and summary
+mean(pwcor)
+median(pwcor)
+summary(pwcor)
